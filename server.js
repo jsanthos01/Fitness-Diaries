5853c770ca3a436f855e495ab73c2bc032e63299
poem <normamoras@gmail.com>
2020-03-10 13:30:55 -0400

    adding to my master table

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require( 'express' );
-
+const express = require("express");
+const orm = require("./orm");
 const PORT = process.env.PORT || 8080;
-const app  = express();
+const app = express();
 
-app.use( express.static('public') );
-app.use( express.urlencoded({ extended: false }) );
-
-
-app.listen( PORT, function(){
-    console.log( `[pictures] RUNNING, http://localhost:${PORT}` );
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.listen(PORT, function () {
+  console.log(`[pictures] RUNNING, http://localhost:${PORT}`);
 });
 
+app.get("/api/user", async function (req, res) {
+  const displayUserBase = await orm.fetchUserBase();
+  res.send(displayUserBase);
+});
