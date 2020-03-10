b489591d50aa2e30e56701680ae7d3b1add12c6c
jsanthos01 <chinu.s2001@gmail.com>
2020-03-10 13:22:43 -0400

    fixed file orders

--- orm.js
+++ orm.js
@@ -24,11 +24,33 @@ class Database {
     }
 }
 
-  // at top INIT DB connection
+// at top INIT DB connection
 const db = new Database({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "bootcamp2020", //change 
-    database: "fitness_diaries" 
+    database: "fitness_diaries"
 });
+
+async function getUsersInfo() {
+    
+}
+
+async function postUsersInfo(){
+
+}
+
+async function storeRegistrationInfo(myPost){
+    const myResult = await db.query( 
+        "INSERT INTO users(first_name,last_name,email_address,user_password) VALUES(?,?,?,?)",
+        [ myPost.first_name, myPost.last_name, myPost.email_address, myPost.user_password ] );
+    return myResult
+    
+}
+module.exports = {
+    storeRegistrationInfo,
+    getUsersInfo,
+    postUsersInfo
+}
+
