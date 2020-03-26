59befe8aa875597e5689e5ef81a49bb762de988f
jsanthos01 <chinu.s2001@gmail.com>
2020-03-26 10:39:32 -0400

    fixing heroku issues

--- orm.js
+++ orm.js
@@ -1,45 +1,45 @@
-var db = require("./connection");
+// var db = require("./config/connection");
 
 
-// const mysql = require("mysql");
+const mysql = require("mysql");
 
-// class Database {
-//     constructor( config ) {
-//         this.connection = mysql.createConnection( config );
-//     }
-//     query( sql, args=[] ) {
-//         return new Promise( ( resolve, reject ) => {
-//             this.connection.query( sql, args, ( err, rows ) => {
-//                 if ( err )
-//                     return reject( err );
-//                 resolve( rows );
-//             } );
-//         } );
-//     }
-//     close() {
-//         return new Promise( ( resolve, reject ) => {
-//             this.connection.end( err => {
-//                 if ( err )
-//                     return reject( err );
-//                 resolve();
-//             } );
-//         } );
-//     }
-// }
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args=[] ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
 
-// var db;
-// if(process.env.JAWSDB_URL){
-//      db = new Database(process.env.JAWSDB_URL);
-//      console.log('jaws db is connected');
-// }else{
-//      db = new Database({
-//         host: "localhost",
-//         port: 3306,
-//         user: "root",
-//         password: "bootcamp2020", //change 
-//         database: "fitness_diaries"
-//     });
-// };
+var db;
+if(process.env.JAWSDB_URL){
+     db = new Database(process.env.JAWSDB_URL);
+     console.log('jaws db is connected');
+}else{
+     db = new Database({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "bootcamp2020", //change 
+        database: "fitness_diaries"
+    });
+};
 
 async function registrationSql(myPost){
     console.log(myPost);
